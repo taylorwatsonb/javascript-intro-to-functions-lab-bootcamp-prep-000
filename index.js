e4a69f01511c7c868879f35108c9bae11ae9868e
Taylor Watson <taylorwatsonb@gmail.com>
2018-08-19 18:06:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,20 @@ function whisper(string)
  }
  
  
+logShout("hello")
+ function whisper(string){
+  return string.toLowerCase();
+}
+ whisper("HELLO")
+ function logWhisper(string){
+  return console.log(string.toLowerCase())
+}
+ logWhisper("HELLO")
+ function sayHiToGrandma(string){
+  if (string === string.toLowerCase()) return "I can't hear you!";
+  else if (string === "I love you, Grandma.") return "I love you, too.";
+  else if (string === string.toUpperCase()) return "YES INDEED!";
+}
+ sayHiToGrandma("hello");
+sayHiToGrandma("HELLO");
+sayHiToGrandma("I love you, Grandma.");
